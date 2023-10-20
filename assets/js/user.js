9240c6c8005151f72ede38686965b774f46d7bc4
dassifr <136333316+dassifr@users.noreply.github.com>
2023-10-20 00:12:11 +0000

    update

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,3 +1,5 @@
+
+console.log("bbbbbbbbbbbbb");
 /*--
         INSCRIPTION
     -----------------------------------*/
@@ -24,7 +26,7 @@ $(document).ready(function() {
       // Effectuez une requête POST vers votre API
       $.ajax({
           type: 'POST',
-          url: 'http://192.168.0.10:3000/users/sinscrire',
+          url: 'http://192.168.31.145:3000/users/sinscrire',
           data: JSON.stringify(formData),
           contentType: 'application/json',
           success: function(response) {
@@ -45,42 +47,133 @@ $(document).ready(function() {
 /*--
         CONNEXION
     -----------------------------------*/
-$(document).ready(function() {
-  $('#loginForm').submit(function(event) {
-      event.preventDefault(); // Empêche le rechargement de la page
+    function setCookie(name, value, days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + JSON.stringify(value) + expires + "; path=/";
+    }
 
-      // Récupérez les valeurs des champs du formulaire
-      var email = $('#email').val();     
-      var password = $('#password').val();
+    // Fonction pour récupérer des données depuis un cookie
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1, cookie.length);
+            }
+            if (cookie.indexOf(nameEQ) === 0) {
+                return JSON.parse(cookie.substring(nameEQ.length, cookie.length));
+            }
+        }
+        return null;
+    }
 
-      // Validez les champs (vous pouvez ajouter des validations ici)
+    var connDataResult = getCookie("connDataResult");
 
-      // Créez un objet contenant les données du formulaire
-      var formData = {
-          email: email,
-          password: password
-      };
+    if (connDataResult) {
 
-      // Effectuez une requête POST vers votre API
-      $.ajax({
-          type: 'POST',
-          url: 'http://192.168.0.10:3000/users/connexion',
-          data: JSON.stringify(formData),
-          contentType: 'application/json',
-          success: function(response) {
-              // Gérez la réponse de l'API (par exemple, affichez un message de succès)
-              console.log('connexion réussie :', response);
-              // Redirigez l'utilisateur vers la page de connexion en cas de succès
-              window.location.href = 'index.html';
-                    },
-          error: function(error) {
-              // Gérez les erreurs de l'API (par exemple, affichez un message d'erreur)
-              console.error('Erreur lors de l\'inscription :', error);
-              alert('Information Incorrete. Veuillez réessayer.');
-          }
-      });
-  });
-});
+        // Utilisez connDataResult comme vous le souhaitez
+        var userDataDiv = document.getElementById("userData");
+        userDataDiv.innerHTML = connDataResult.firstName;
+    }
+    
+
+    $(document).ready(function() {
+        $('#loginForm').submit(function(event) {
+            event.preventDefault(); // Empêche le rechargement de la page
+
+            // Récupérez les valeurs des champs du formulaire
+            var email = $('#email').val();
+            var password = $('#password').val();
+
+            // Validez les champs (vous pouvez ajouter des validations ici)
+
+            // Créez un objet contenant les données du formulaire
+            var formData = {
+                email: email,
+                password: password
+            };
+
+            // Effectuez une requête POST vers votre API
+            $.ajax({
+                type: 'POST',
+                url: 'http://192.168.31.145:3000/users/connexion',
+                data: JSON.stringify(formData),
+                contentType: 'application/json',
+                success: function(response) {
+                    // Gérez la réponse de l'API (par exemple, affichez un message de succès)
+                    console.log('Connexion réussie :', response);
+                    connDataResult = response.user;
+
+                    // Stockez connDataResult dans un cookie
+                    setCookie("connDataResult", connDataResult, 30); // 30 jours d'expiration
+
+                    // Mettez à jour l'élément HTML avec le nom de l'utilisateur
+                  
+
+                    // Redirigez l'utilisateur vers la page de connexion en cas de succès
+                    window.location.href = 'index.html';
+                },
+                error: function(error) {
+                    // Gérez les erreurs de l'API (par exemple, affichez un message d'erreur)
+                    console.error('Erreur lors de la connexion :', error);
+                    alert('Informations incorrectes. Veuillez réessayer.');
+                }
+            });
+        });
+    });
+
+
+// $(document).ready(function() {
+//   $('#loginForm').submit(function(event) {
+//       event.preventDefault(); // Empêche le rechargement de la page
+
+//       // Récupérez les valeurs des champs du formulaire
+//       var email = $('#email').val();     
+//       var password = $('#password').val();
+
+//       // Validez les champs (vous pouvez ajouter des validations ici)
+
+//       // Créez un objet contenant les données du formulaire
+//       var formData = {
+//           email: email,
+//           password: password
+//       };
+
+//       // Effectuez une requête POST vers votre API
+//       $.ajax({
+//           type: 'POST',
+//           url: 'http://192.168.31.145:3000/users/connexion',
+//           data: JSON.stringify(formData),
+//           contentType: 'application/json',
+//           success: function(response) {
+//               // Gérez la réponse de l'API (par exemple, affichez un message de succès)
+//               console.log('connexion réussie :', response);
+//               connDataResult=response.user;
+//               userName = connDataResult.firstName;
+//               var strongElement = document.getElementById("theusername");
+//               strongElement.innerHTML = userName;
+
+
+//               console.log(connDataResult);
+//               // Redirigez l'utilisateur vers la page de connexion en cas de succès
+//                window.location.href = 'index.html';
+//                     },
+//           error: function(error) {
+//               // Gérez les erreurs de l'API (par exemple, affichez un message d'erreur)
+//               console.error('Erreur lors de l\'inscription :', error);
+//               alert('Information Incorrete. Veuillez réessayer.');
+//           }
+//       });
+
+      
+//   });
+// });
 
 /*--
         CACHER BUTTON
